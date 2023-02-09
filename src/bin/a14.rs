0ab231ea10ef38573d4204347e2f274d5a048302
greenlink <greenlink@users.noreply.github.com>
2023-02-09 10:55:53 -0300

    modify: solve exercise a14.

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,34 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+fn print_text(text: &str) {
+    println!("{text}");
+}
+
+struct Person {
+    name: String,
+    age: u32,
+    favorite_color: String
+}
+
+impl Person {
+    fn print_person(&self) {
+        print_text(&["Person's name: ", &self.name].concat());
+        print_text(&["Person's favorite color: ", &self.favorite_color].concat());
+    }
+}
+
+fn main() {
+    let people = vec![
+        Person {name: "Alice".to_string(), age: 9, favorite_color: "Lilac".to_string()},
+        Person {name: "Robert".to_string(), age: 33, favorite_color: "Blue".to_string()},
+        Person {name: "Priscila".to_string(), age: 10, favorite_color: "Amaryllis".to_string()},
+    ];
+
+    for person in &people {
+        if person.age <= 10 {
+            person.print_person();
+        }
+    }
+}
+
