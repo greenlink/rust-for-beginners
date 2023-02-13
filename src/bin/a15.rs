3b3328dc64f53bbafbe717a32852469ac7fb6f21
greenlink <greenlink@users.noreply.github.com>
2023-02-13 13:25:45 -0300

    modify: solve exercise a15.

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -11,4 +11,30 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Tickets {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64),
+}
+
+fn main() {
+    let tickets = vec![
+        Tickets::Backstage(180.0, "Dylan".to_owned()),
+        Tickets::Vip(120.0, "Maura".to_owned()),
+        Tickets::Standard(80.0),
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Tickets::Standard(price) => println!("Standard holder, price is U$ {};", price),
+            Tickets::Vip(price, holder) => {
+                println!("Vip holder is called {1}, price is U$ {0};", price, holder)
+            }
+            Tickets::Backstage(price, holder) => println!(
+                "Backstage holder is called {1}, price is U$ {0};",
+                price, holder
+            ),
+        }
+    }
+}
+
