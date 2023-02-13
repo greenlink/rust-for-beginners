6e863969e73085935cc8159af10c8c746ace2bf2
greenlink <greenlink@users.noreply.github.com>
2023-02-13 13:51:01 -0300

    modify: solve exercise a16.

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -8,7 +8,26 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker: Option<i32>
+}
 
-fn main() {}
+impl Student {
+    fn print(&self) {
+        print!("Student's name is {}, ", self.name);
+        match self.locker {
+            Some(number) => println!("locker {} assigned.", number),
+            None => println!("have no Locker.")
+        };
+    }
+}
+
+fn main() {
+    let mark = Student {name: String::from("Mark"), locker: Some(14)};
+    let helly = Student {name: String::from("Helly"), locker: None};
+
+    mark.print();
+    helly.print();
+}
+
