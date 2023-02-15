4e8c04344c3a9ceabe637955fdc3816e80e89d41
greenlink <greenlink@users.noreply.github.com>
2023-02-15 09:23:43 -0300

    modify: solve exercise a18.

--- src/bin/a18.rs
+++ src/bin/a18.rs
@@ -11,4 +11,33 @@
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
 
-fn main() {}
+struct Customer {
+    name: String,
+    age: u8
+}
+
+impl Customer {
+    fn is_customer_adult(&self) -> Result<String, String> {
+        if self.age >= 21 {
+            return Ok("is legally adult.".to_owned());
+        }
+
+        return Err("is underaged.".to_owned());
+    }
+
+    fn can_purchase(&self) {
+        match self.is_customer_adult() {
+            Ok(msg) => println!("Customer named {0} {1}", self.name, msg),
+            Err(error) => println!("Customer named {0} {1}", self.name, error)
+        }
+    }
+}
+
+fn main() {
+    let harry = Customer {name: "Harry".to_owned(), age: 12};
+    let megan = Customer {name: "Megan".to_owned(), age: 24};
+    
+    harry.can_purchase();
+    megan.can_purchase();
+}
+
