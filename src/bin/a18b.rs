4e9be3505fa0be13e1994944b5e40a102f011282
greenlink <greenlink@users.noreply.github.com>
2023-02-15 13:35:01 -0300

    modify: solve exercise a18b.

--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -22,4 +22,68 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+enum EmployeePosition {
+    AssemblyTechnicians,
+    KitchenStaff,
+    LineSupervisor,
+    MaintenanceCrew,
+    Manager,
+    MarketingDepartment,
+}
+
+struct Employee {
+    employee_position: EmployeePosition,
+    employed: bool,
+}
+
+impl Employee {
+    fn check_access(&self) -> Result<(), String> {
+        let no_access = "Employee can't access the building.".to_owned();
+
+        if !self.employed {
+            return Err(no_access);
+        }
+
+        match self.employee_position {
+            EmployeePosition::MaintenanceCrew => return Ok(()),
+            EmployeePosition::Manager => return Ok(()),
+            EmployeePosition::MarketingDepartment => return Ok(()),
+            _ => return Err(no_access),
+        }
+    }
+
+    fn print_access(&self) -> Result<(), String> {
+        self.check_access()?;
+        println!("Employee can access the building.");
+        Ok(())
+    }
+}
+
+fn main() {
+    let manager = Employee {
+        employee_position: EmployeePosition::Manager,
+        employed: true,
+    };
+    let manager_fired = Employee {
+        employee_position: EmployeePosition::Manager,
+        employed: false,
+    };
+    let staff = Employee {
+        employee_position: EmployeePosition::KitchenStaff,
+        employed: true,
+    };
+    let staff_fired = Employee {
+        employee_position: EmployeePosition::KitchenStaff,
+        employed: false,
+    };
+
+    let msg1 = manager.print_access();
+    println!("{:?}", msg1);
+    let msg2 = manager_fired.print_access();
+    println!("{:?}", msg2);
+    let msg3 =staff.print_access();
+    println!("{:?}", msg3);
+    let msg4 = staff_fired.print_access();
+    println!("{:?}", msg4);
+}
+
