d1fd669bc792fe39db18a9f455a3074213c3cf61
greenlink <5113785+greenlink@users.noreply.github.com>
2024-08-07 14:51:19 -0300

    add: project 1.

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -18,4 +18,154 @@
 // * Create your program starting at level 1. Once finished, advance to the
 //   next level.
 
-fn main() {}
+use std::collections::HashMap;
+use std::io::stdin;
+use crate::Menu::{AddBill, Exit, InvalidOption, RemoveBill, ShowBills, EditBill};
+
+fn main() {
+    let mut bills = HashMap::new();
+    let mut menu_option = InvalidOption;
+    while !matches!(menu_option, Exit) {
+        Menu::show_menu();
+        let menu_string =  get_command();
+        menu_option = Menu::get_menu_option(menu_string);
+        println!("****************");
+
+        match menu_option {
+            AddBill => Menu::add_bill(&mut bills),
+            ShowBills => Menu::show_bills(&mut bills),
+            RemoveBill => Menu::remove_bill(&mut bills),
+            EditBill => Menu::edit_bill(&mut bills),
+            Exit => println!("Exiting application."),
+            _ => {}
+        }
+    }
+}
+
+enum Menu{
+    InvalidOption = 0,
+    AddBill = 1,
+    ShowBills = 2,
+    RemoveBill = 3,
+    EditBill = 4,
+    Exit = 99,
+}
+
+impl Menu {
+    fn show_menu(){
+        println!("***** MENU *****");
+        println!("1 - Add bill");
+        println!("2 - View bills");
+        println!("3 - Remove bill");
+        println!("4 - Edit bill");
+        println!("99 - Exit");
+    }
+
+    fn get_menu_option(menu_option_str: String) -> Menu {
+        let menu_option_parsed = menu_option_str.parse::<isize>().unwrap_or_else(|error| {
+            println!("Error while parsing the chosen option: {error}");
+            0
+        });
+        match menu_option_parsed {
+            1 => AddBill,
+            2 => ShowBills,
+            3 => RemoveBill,
+            4 => EditBill,
+            99 => Exit,
+            _ => {
+                println!("Invalid option.");
+                InvalidOption
+            }
+        }
+    }
+
+    fn add_bill(bills: &mut HashMap<usize, Bill>){
+        println!("Enter the name of the bill:");
+        let name = get_command();
+        println!("Enter the amount to be paid(use . for decimals):");
+        let amount = get_command().parse::<f32>().unwrap_or_else(|error| {
+            println!("Error while parsing the amount: {error}");
+            0f32
+        });
+
+        if !name.is_empty() && amount != 0f32 {
+            let new_bill_key = match bills.keys().max() {
+                Some(result) => result + 1,
+                None => 1
+            };
+            bills.insert(new_bill_key, Bill::new(name, amount));
+            println!("Bill added successfully!");
+        }
+    }
+
+    fn show_bills(bills: & HashMap<usize, Bill>){
+        for item in bills.iter() {
+            let bill = item.1;
+            println!("{} - Name: {}, Amount: ${}", item.0, bill.name, bill.amount.to_string())
+        }
+    }
+
+    fn remove_bill(bills: &mut HashMap<usize, Bill>) {
+        Self::show_bills(&bills);
+        println!("Enter the number of the bill:");
+        let bill_number = get_command().parse::<usize>().unwrap_or_else(|error| {
+            println!("Error while parsing the amount: {error}");
+            0
+        });
+
+        if bills.contains_key(&bill_number) {
+            bills.remove(&bill_number);
+        } else {
+            println!("Invalid bill number.");
+        }
+    }
+
+    fn edit_bill(bills: &mut HashMap<usize, Bill>) {
+        Self::show_bills(&bills);
+        println!("Enter the number of the bill:");
+        let bill_number = get_command().parse::<usize>().unwrap_or_else(|error| {
+            println!("Error while parsing the amount: {error}");
+            0
+        });
+
+        if bills.contains_key(&bill_number) {
+            println!("Enter the new name of the bill:");
+            let name = get_command();
+            println!("Enter the new amount to be paid(use . for decimals):");
+            let amount = get_command().parse::<f32>().unwrap_or_else(|error| {
+                println!("Error while parsing the amount: {error}");
+                0f32
+            });
+
+            bills.get_mut(&bill_number).unwrap().name = name;
+            bills.get_mut(&bill_number).unwrap().amount = amount;
+        } else {
+            println!("Invalid bill number.");
+        }
+    }
+}
+
+struct Bill{
+    name: String,
+    amount: f32,
+}
+
+impl Bill {
+    fn new(name: String, amount: f32) -> Self{
+        Bill{
+            name,
+            amount
+        }
+    }
+}
+
+fn get_command() -> String{
+    let mut input = String::new();
+
+    match stdin().read_line(&mut input) {
+        Ok(_) => (),
+        Err(error) => println!("Error while trying to get input: {error}"),
+    }
+
+    input.trim().to_string()
+}
