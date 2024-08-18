554b1e1692b02415c2ae52640ad7c1ae8460c344
Greenlink <lennon@sistemasbr.net>
2024-08-18 11:53:05 -0300

    add: exercise a25.

--- src/bin/a25.rs
+++ src/bin/a25.rs
@@ -10,6 +10,39 @@
 // Notes:
 // * Use a trait to declare a perimeter calculation function
 // * Use a single function to print out the perimeter of the shapes
-//   * The function must utilize impl trait as a function parameter
+// * The function must utilize impl trait as a function parameter
 
-fn main() {}
+struct Square{
+    side : i32,
+}
+
+struct Triangle{
+    side_a : i32,
+    side_b : i32,
+    side_c : i32,
+}
+
+trait Shape {
+    fn get_perimeter(&self) -> i32;
+}
+
+impl Shape for Triangle {
+    fn get_perimeter(&self) -> i32 {
+        self.side_a+self.side_b+self.side_c
+    }
+}
+
+impl Shape for Square {
+    fn get_perimeter(&self) -> i32 {
+        self.side*4
+    }
+}
+
+fn show_perimeter(thing: impl Shape){
+    println!("Perimeter: {:?}", thing.get_perimeter())
+}
+
+fn main() {
+    show_perimeter(Triangle{side_a: 2, side_b: 3, side_c: 4});
+    show_perimeter(Square{side: 2});
+}
