7dea15aaf903bf6b37ebc4357e7ab828f15ec9d6
greenlink <5113785+greenlink@users.noreply.github.com>
2024-08-19 13:14:20 -0300

    add: exercise a26.

--- src/bin/a26.rs
+++ src/bin/a26.rs
@@ -7,5 +7,8 @@
 // * Use the `chrono` crate to work with time
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
+use chrono::*;
 
-fn main() {}
+fn main() {
+    println!("{}", Local::now().to_string());
+}
